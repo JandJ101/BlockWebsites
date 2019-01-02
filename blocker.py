82d89fa3f80b2d1fb17d7d0bc66f79ee6c3c9763
Tobias <t-render-man@sc.rr.com>
2019-01-01 20:08:13 -0500

    cleaned up some code

--- blocker.py
+++ blocker.py
@@ -17,7 +17,7 @@ def findTextLine(text):
     for i, line in enumerate(open(hostsPath), 1):
         if text in line:
             x = i
-    return(x)
+    return (x)
 
 
 waitTime = int(config.time)
@@ -26,68 +26,58 @@ if "#timetillend" in open(hostsPath).read():
     fileToExtract = open(hostsPath).readlines()
     lineOftime = fileToExtract[lineNum - 1]
     newTime = int(lineOftime.split("#timetillend", 1)[1])
-    print("Your time is still " + str(newTime) + " minute(s) until timer completes")
+    print("Your time is still " + str(newTime) +
+          " minute(s) until timer completes")
     waitTime = newTime
 
-
-input("This program will restart all web browsers. Press enter to continue.")
-
-def killByProcessName(name):
-    PROCNAME = 'python.exe'
-    for proc in psutil.process_iter():
-        if proc.name == PROCNAME:
-            p = psutil.Process(proc.pid)
-            if not 'SYSTEM' in p.username:
-                proc.kill()
-
-    #print("Not found process: " + name)
-
+input("\nThis program will restart all web browsers. Press enter to continue.")
 
 def closeBrowsers():
-    #print(os.name)
     browsersToClose = config.webBrowsers
 
     if platform.system() == "Darwin":
         print("macos")
-        i=0
+        i = 0
         while i < len(browsersToClose):
             if browsersToClose[i] in (p.name() for p in psutil.process_iter()):
                 print(browsersToClose[i] + " is running")
-                os.system("killall " + browsersToClose[i].replace(" ", '\\ ' ))
+                os.system("killall " + browsersToClose[i].replace(" ", '\\ '))
                 print("Closed " + browsersToClose[i])
-            i+=1
+            i += 1
 
     if platform.system() == "Windows":
-        i=0
+        i = 0
         while i < len(browsersToClose):
             if browsersToClose[i] in (p.name() for p in psutil.process_iter()):
                 print(browsersToClose[i] + " is running")
                 os.system("TASKKILL /F /IM " + browsersToClose[i])
                 print("Closed " + browsersToClose[i])
-            i+=1
+            i += 1
+
 
 closeBrowsers()
-
 
 toBlock = config.blockSites
 print("These sites will be blocked.")
 print(toBlock)
 
+
 def compileList():
-    i=0
+    i = 0
     compiledDump = "#blockSites\n"
     while i < len(toBlock):
         compiledDump += "127.0.0.1 " + toBlock[i] + "\n"
         compiledDump += "127.0.0.1 www." + toBlock[i] + "\n"
-        i+=1
+        i += 1
 
-    compiledDump += "#timetillend"+ str(waitTime) +"\n"
+    compiledDump += "#timetillend" + str(waitTime) + "\n"
     compiledDump += "#endBlockSites\n"
-    return(compiledDump)
+    return (compiledDump)
 
 
 def removeLines():
-    if "#blockSites" in open(hostsPath).read() and "#endBlockSites" in open(hostsPath).read():
+    if "#blockSites" in open(hostsPath).read() and "#endBlockSites" in open(
+            hostsPath).read():
         oldLines = hostsFileRead
         newLines = ""
 
@@ -101,7 +91,7 @@ def removeLines():
         while i < len(oldLines):
             if i not in deleteThese:
                 newLines += oldLines[i]
-            i+=1
+            i += 1
 
         raw = open(hostsPath, "r+")
         raw.seek(0)
@@ -109,18 +99,16 @@ def removeLines():
         raw.write(newLines)
 
 
-
 def addBlockList():
     removeLines()
     x = open(hostsPath, "a")
     toWrite = compileList()
 
-
     x.write(toWrite)
 
 
 def countdown(t):
-    t*=60
+    t *= 60
     print("Time till finished:")
     while t:
         mins, secs = divmod(t, 60)
@@ -129,7 +117,6 @@ def countdown(t):
         time.sleep(1)
         t -= 1
     print('00:00\nBlock ended.')
-
 
 
 addBlockList()
